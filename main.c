a86ba2b304bdabc78a8d96103c9174be6fffd47b
araborn99 <45438623+araborn99@users.noreply.github.com>
2020-03-09 19:19:18 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,7 @@
 /******************************************************************************
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
+                            Made by Alex Raborn
+                Dr. Linken shows a flow chart, I create a program.
 
 *******************************************************************************/
 
